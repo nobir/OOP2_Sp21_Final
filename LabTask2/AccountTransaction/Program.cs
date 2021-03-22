4c87c0ef5009d92669da8e2ed638bf45f9b01241
Nobir <nobirfreelancer@gmail.com>
2021-03-22 12:04:40 +0600

    Account Transaction task completed

--- LabTask2/AccountTransaction/Program.cs
+++ LabTask2/AccountTransaction/Program.cs
@@ -10,6 +10,22 @@ namespace AccountTransaction
     {
         static void Main(string[] args)
         {
+            var a0 = new Account("Nobir 1", "a000", 1000);
+            var a1 = new Account("Nobir 2", "a111", 2000);
+
+            a0.Deposit(10000);
+            a0.Withdraw(5000);
+            a0.Transfer(a1, 4000);
+
+            Console.WriteLine("\n====== Account Balance ======\n");
+
+            a0.ShowInfo();
+            a1.ShowInfo();
+
+            Console.WriteLine("\n====== Account Transactions List ======\n");
+
+            a0.ShowAllTransaction();
+            a1.ShowAllTransaction();
         }
     }
 }
