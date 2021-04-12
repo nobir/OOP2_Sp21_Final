ad7b6c06d46d91f9ae539bfa3ca4bb85a65e1d5e
Nobir <nobirfreelancer@gmail.com>
2021-04-12 13:09:39 +0600

    Lab 5 Task 1

---
+++ Lab5/Task1/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Task1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbCCname = new System.Windows.Forms.TextBox();
+            this.tbCCode = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCLoad = new System.Windows.Forms.Button();
+            this.dgvCourses = new System.Windows.Forms.DataGridView();
+            this.btnCAdd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCourses)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 52);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Course Name:";
+            // 
+            // tbCCname
+            // 
+            this.tbCCname.Location = new System.Drawing.Point(218, 46);
+            this.tbCCname.Name = "tbCCname";
+            this.tbCCname.Size = new System.Drawing.Size(376, 29);
+            this.tbCCname.TabIndex = 1;
+            // 
+            // tbCCode
+            // 
+            this.tbCCode.Location = new System.Drawing.Point(218, 106);
+            this.tbCCode.Name = "tbCCode";
+            this.tbCCode.Size = new System.Drawing.Size(376, 29);
+            this.tbCCode.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 112);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Course Code:";
+            // 
+            // btnCLoad
+            // 
+            this.btnCLoad.Location = new System.Drawing.Point(38, 169);
+            this.btnCLoad.Name = "btnCLoad";
+            this.btnCLoad.Size = new System.Drawing.Size(172, 43);
+            this.btnCLoad.TabIndex = 4;
+            this.btnCLoad.Text = "Load";
+            this.btnCLoad.UseVisualStyleBackColor = true;
+            this.btnCLoad.Click += new System.EventHandler(this.btnCLoad_Click);
+            // 
+            // dgvCourses
+            // 
+            this.dgvCourses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCourses.Location = new System.Drawing.Point(38, 259);
+            this.dgvCourses.Name = "dgvCourses";
+            this.dgvCourses.RowTemplate.Height = 24;
+            this.dgvCourses.Size = new System.Drawing.Size(723, 211);
+            this.dgvCourses.TabIndex = 5;
+            // 
+            // btnCAdd
+            // 
+            this.btnCAdd.Location = new System.Drawing.Point(218, 169);
+            this.btnCAdd.Name = "btnCAdd";
+            this.btnCAdd.Size = new System.Drawing.Size(172, 43);
+            this.btnCAdd.TabIndex = 4;
+            this.btnCAdd.Text = "Add";
+            this.btnCAdd.UseVisualStyleBackColor = true;
+            this.btnCAdd.Click += new System.EventHandler(this.btnCAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 525);
+            this.Controls.Add(this.btnCAdd);
+            this.Controls.Add(this.dgvCourses);
+            this.Controls.Add(this.btnCLoad);
+            this.Controls.Add(this.tbCCode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbCCname);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCourses)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbCCname;
+        private System.Windows.Forms.TextBox tbCCode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCLoad;
+        private System.Windows.Forms.DataGridView dgvCourses;
+        private System.Windows.Forms.Button btnCAdd;
+    }
+}
+
---
+++ Lab5/Task1/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Collections;
+
+namespace Task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private SqlConnection ConnectDb()
+        {
+            SqlConnection connection = null;
+
+            try{
+                string connString = @"Server=NOBIR\SQLEXPRESS;Database=db_final_sp;Integrated Security=true;";
+                connection = new SqlConnection(connString);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("[Error]\n" + ex + "\n");
+            }
+
+            return connection;
+        }
+
+        private void btnCLoad_Click(object sender, EventArgs e)
+        {
+            this.ShowAllCourse();
+        }
+
+        private void btnCAdd_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = this.ConnectDb();
+
+            if (connection == null)
+            {
+                MessageBox.Show("Connection Failed", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            connection.Open();
+
+            string errorMessage = "";
+
+            // Form data
+            string cname = tbCCname.Text.Trim();
+            string ccode = tbCCode.Text.Trim();
+
+            // MySQL query
+            string query = String.Format("INSERT INTO course VALUES('{0}','{1}')", ccode, cname);
+
+            // validation
+            if(cname.Length == 0)
+            {
+                errorMessage += "\nCourse Name Can't be Empty.";
+            }
+            if(ccode.Length == 0)
+            {
+                errorMessage += "\n\nCourse Code Can't be Empty.";
+            }
+
+            // show error message if available
+            if(errorMessage.Length != 0)
+            {
+                MessageBox.Show(errorMessage, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand(query, connection);
+                int row = cmd.ExecuteNonQuery();
+
+
+                if(row > 0)
+                {
+                    MessageBox.Show("Successfully Added", "Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Failed to Add", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            connection.Close();
+            this.ShowAllCourse();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ShowAllCourse();
+        }
+
+        private void ShowAllCourse()
+        {
+            SqlConnection connection = this.ConnectDb();
+
+            if (connection == null)
+            {
+                MessageBox.Show("Connection Failed", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // MySQL query
+            string query = @"SELECT * FROM course";
+
+            List<Course> courses = new List<Course>();
+
+            connection.Open();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand(query, connection);
+                SqlDataReader data = cmd.ExecuteReader();
+
+                while (data.Read())
+                {
+                    Course c = new Course();
+
+                    c.Id = data.GetInt32(data.GetOrdinal("id"));
+                    c.CourseName = data.GetString(data.GetOrdinal("cname"));
+                    c.CourseCode = data.GetString(data.GetOrdinal("ccode"));
+
+                    courses.Add(c);
+                }
+            }
+            catch (Exception ex)
+            {
+                connection.Close();
+                MessageBox.Show("" + ex, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            connection.Close();
+
+            dgvCourses.DataSource = courses;
+        }
+    }
+}
